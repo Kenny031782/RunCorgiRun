d65870a96e699ea748fa73bd8e0ed45e41165350
Kenny031782 <ken051782@gmail.com>
2026-04-02 15:41:49 -0700

    created gameOverScreen, and made the buttons work

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,22 +3,40 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public UI Ui;
+    public GameTimer GameTimer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Ui.HideGameOverScreen();
+        Ui.ShowStartScreen();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ui.ShowTime();
     }
 
     public void OnStartButtonClicked()
     {
         // hide StartScreen
         Ui.HideStartScreen();
-        // start game
+        StartGame();
+    }
+
+    public void OnPlayAgainButtonClicked()
+    {
+        Ui.HideGameOverScreen();
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        GameTimer.StartTimer(10, OnTimerFinished);
+    }
+
+    private void OnTimerFinished()
+    {
+        Ui.ShowGameOverScreen();
     }
 }
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    public TMP_Text TimeText;
     public TMP_Text scoreText;
     public CanvasGroup StartScreenCanvasGroup;
+    public CanvasGroup GameOverScreenCanvasGroup;
+    public GameTimer GameTimer;
     public void SetScoreText(int score)
     {
         scoreText.text = "Score: " + score;
@@ -13,5 +16,24 @@ public class UI : MonoBehaviour
     public void HideStartScreen()
     {
         CanvasGroupDisplayer.Hide(StartScreenCanvasGroup);
+    }
+    
+    public void HideGameOverScreen() {
+        CanvasGroupDisplayer.Hide(GameOverScreenCanvasGroup);
+
+    }
+    
+    public void ShowGameOverScreen() {
+        CanvasGroupDisplayer.Show(GameOverScreenCanvasGroup);
+
+    }
+    public void ShowStartScreen() {
+        CanvasGroupDisplayer.Show(StartScreenCanvasGroup);
+
+    }
+
+    public void ShowTime()
+    {
+        TimeText.text = GameTimer.GetTimeAsString();
     }
 }
